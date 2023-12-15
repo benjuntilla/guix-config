0bd6b15dbfd8ba064a92c5f615c779284e145c48
Ben Juntilla <46634820+benjuntilla@users.noreply.github.com>
2023-12-14 20:21:43 -0700

    fix channels

--- home-configuration.scm
+++ home-configuration.scm
@@ -239,44 +239,44 @@
 	(service home-dbus-service-type)
 	(service home-batsignal-service-type)
 	(service home-emacs-service-type)
-	(simple-service 'extra-channels-service
-					home-channels-service-type
-					(list (channel
-							(name 'guix)
-							(url "https://git.savannah.gnu.org/git/guix.git")
-							(branch "master")
-							(introduction
-							  (make-channel-introduction
-								"9edb3f66fd807b096b48283debdcddccfea34bad"
-								(openpgp-fingerprint
-								  "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
-						  (channel
-							(name 'guix-gaming-games)
-							(url "https://gitlab.com/guix-gaming-channels/games.git")
-							;; Enable signature verification:
-							(introduction
-							  (make-channel-introduction
-								"c23d64f1b8cc086659f8781b27ab6c7314c5cca5"
-								(openpgp-fingerprint
-								  "50F3 3E2E 5B0C 3D90 0424  ABE8 9BDC F497 A4BB CC7F"))))
-						  (channel
-							(name 'small-guix)
-							(url "https://gitlab.com/orang3/small-guix")
-							;; Enable signature verification:
-							(introduction
-							  (make-channel-introduction
-								"940e21366a8c986d1e10a851c7ce62223b6891ef"
-								(openpgp-fingerprint
-								  "D088 4467 87F7 CBB2 AE08  BE6D D075 F59A 4805 49C3"))))
-						  (channel
-							(name 'nonguix)
-							(url "https://gitlab.com/nonguix/nonguix")
-							(branch "master")
-							(introduction
-							  (make-channel-introduction
-								"897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-								(openpgp-fingerprint
-								  "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))))
+  (simple-service 'extra-channels-service
+                  home-channels-service-type
+                  (list (channel
+                         (name 'guix)
+                         (url "https://git.savannah.gnu.org/git/guix.git")
+                         (branch "master")
+                         (introduction
+                          (make-channel-introduction
+                           "9edb3f66fd807b096b48283debdcddccfea34bad"
+                           (openpgp-fingerprint
+                            "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+                        (channel
+                         (name 'guix-gaming-games)
+                         (url "https://gitlab.com/guix-gaming-channels/games.git")
+                         ;; Enable signature verification:
+                         (introduction
+                          (make-channel-introduction
+                           "c23d64f1b8cc086659f8781b27ab6c7314c5cca5"
+                           (openpgp-fingerprint
+                            "50F3 3E2E 5B0C 3D90 0424  ABE8 9BDC F497 A4BB CC7F"))))
+                        (channel
+                         (name 'small-guix)
+                         (url "https://gitlab.com/orang3/small-guix")
+                         ;; Enable signature verification:
+                         (introduction
+                          (make-channel-introduction
+                           "f260da13666cd41ae3202270784e61e062a3999c"
+                           (openpgp-fingerprint
+                            "8D10 60B9 6BB8 292E 829B  7249 AED4 1CC1 93B7 01E2"))))
+                        (channel
+                         (name 'nonguix)
+                         (url "https://gitlab.com/nonguix/nonguix")
+                         (branch "master")
+                         (introduction
+                          (make-channel-introduction
+                           "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+                           (openpgp-fingerprint
+                            "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))))
     (service home-gpg-agent-service-type
              (home-gpg-agent-configuration
               (pinentry-program
