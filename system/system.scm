f12f199973e2786c74058c817fb61c59e6440db6
Ben Juntilla <ben@benjuntilla.com>
2024-05-11 23:27:17 -0700

    chore/feat: edit whitespace, add firmware service

--- system/system.scm
+++ system/system.scm
@@ -9,90 +9,86 @@
 
 ;; Indicate which modules to import to access the variables
 ;; used in this configuration.
-(use-modules (gnu) (nongnu packages linux))
+(use-modules (gnu) (nongnu packages linux) (nongnu packages firmware))
 (use-service-modules cups desktop networking ssh xorg pm dbus virtualization)
 (use-package-modules wm shells security-token cups)
 
 (operating-system
-  (kernel linux)
-  (firmware (list linux-firmware))
-  (locale "en_US.utf8")
-  (timezone "America/Phoenix")
-  (keyboard-layout (keyboard-layout "us"))
-  (host-name "benslab")
+ (kernel linux)
+ (firmware (list linux-firmware))
+ (locale "en_US.utf8")
+ (timezone "America/Phoenix")
+ (keyboard-layout (keyboard-layout "us"))
+ (host-name "benslab")
 
-  ;; The list of user accounts ('root' is implicit).
-  (users (cons* (user-account
-                  (name "ben")
-                  (comment "Ben")
-                  (group "users")
-                  (home-directory "/home/ben")
-                  (shell (file-append zsh "/bin/zsh"))
-                  (supplementary-groups '("wheel" "netdev" "audio" "video" "plugdev" "libvirt" "kvm")))
-                %base-user-accounts))
+ ;; The list of user accounts ('root' is implicit).
+ (users (cons* (user-account
+                (name "ben")
+                (comment "Ben")
+                (group "users")
+                (home-directory "/home/ben")
+                (shell (file-append zsh "/bin/zsh"))
+                (supplementary-groups '("wheel" "netdev" "audio" "video" "plugdev" "libvirt" "kvm")))
+               %base-user-accounts))
 
-  ;; Packages installed system-wide.  Users can also install packages
-  ;; under their own account: use 'guix search KEYWORD' to search
-  ;; for packages and 'guix install PACKAGE' to install a package.
-  (packages (append (list (specification->package "emacs")
-                          (specification->package "nss-certs"))
-                    %base-packages))
+ (services
+  (cons*
+   (simple-service 'fwupd-dbus dbus-root-service-type
+                   (list fwupd-nonfree))
+   (service openssh-service-type)
+	 (service libvirt-service-type
+            (libvirt-configuration
+             (unix-sock-group "libvirt")))
+   (service cups-service-type
+            (cups-configuration
+             (web-interface? #t)
+             (extensions
+              (list cups-filters hplip-minimal))))
+	 (udev-rules-service 'fido2 libfido2 #:groups '("plugdev"))
+   (service screen-locker-service-type
+            (screen-locker-configuration
+             (name "swaylock")
+             (program (file-append swaylock "/bin/swaylock"))
+             (using-pam? #t)
+             (using-setuid? #f)))
+   (service tlp-service-type
+            (tlp-configuration
+             (cpu-scaling-governor-on-ac (list "performance"))
+             (sched-powersave-on-bat? #t)))
+   (service bluetooth-service-type)
+   ;;          (set-xorg-configuration
+   ;;           (xorg-configuration (keyboard-layout keyboard-layout)))
+   (modify-services %desktop-services
+                    (delete gdm-service-type)
+                    (guix-service-type config => (guix-configuration
+                                                  (inherit config)
+                                                  (substitute-urls
+                                                   (append (list "https://substitutes.nonguix.org")
+                                                           %default-substitute-urls))
+                                                  (authorized-keys
+                                                   (append (list (local-file "./nonguix-signing-key.pub"))
+                                                           %default-authorized-guix-keys)))))))
 
-  (services 
-   (cons* (service openssh-service-type)
-		  (service libvirt-service-type
-				   (libvirt-configuration
-					 (unix-sock-group "libvirt")))
-          (service cups-service-type
-		           (cups-configuration
-					 (web-interface? #t)
-					 (extensions
-    					 (list cups-filters hplip-minimal))))
-		  (udev-rules-service 'fido2 libfido2 #:groups '("plugdev"))
-          (service screen-locker-service-type
-                   (screen-locker-configuration
-                    (name "swaylock")
-                    (program (file-append swaylock "/bin/swaylock"))
-                    (using-pam? #t)
-                    (using-setuid? #f)))
-          (service tlp-service-type
-                   (tlp-configuration
-                    (cpu-scaling-governor-on-ac (list "performance"))
-                    (sched-powersave-on-bat? #t)))
-          (service bluetooth-service-type)
-;;          (set-xorg-configuration
-;;           (xorg-configuration (keyboard-layout keyboard-layout)))
-          (modify-services %desktop-services
-                           (delete gdm-service-type)
-                           (guix-service-type config => (guix-configuration
-                                                         (inherit config)
-                                                         (substitute-urls
-                                                          (append (list "https://substitutes.nonguix.org")
-                                                                  %default-substitute-urls))
-                                                         (authorized-keys
-                                                          (append (list (local-file "./nonguix-signing-key.pub"))
-                                                                  %default-authorized-guix-keys)))))))
+ (bootloader (bootloader-configuration
+              (bootloader grub-efi-bootloader)
+              (targets (list "/boot/efi"))
+              (keyboard-layout keyboard-layout)))
+ (mapped-devices (list (mapped-device
+                        (source (uuid
+                                 "55a0d1ee-fda8-4db4-82d2-1aa41fbf06b4"))
+                        (target "cryptroot")
+                        (type luks-device-mapping))))
 
-  (bootloader (bootloader-configuration
-               (bootloader grub-efi-bootloader)
-               (targets (list "/boot/efi"))
-               (keyboard-layout keyboard-layout)))
-  (mapped-devices (list (mapped-device
-                         (source (uuid
-                                  "55a0d1ee-fda8-4db4-82d2-1aa41fbf06b4"))
-                         (target "cryptroot")
-                         (type luks-device-mapping))))
-
-  ;; The list of file systems that get "mounted".  The unique
-  ;; file system identifiers there ("UUIDs") can be obtained
-  ;; by running 'blkid' in a terminal.
-  (file-systems (cons* (file-system
-                         (mount-point "/boot/efi")
-                         (device (uuid "E342-DBD5"
-                                       'fat32))
-                         (type "vfat"))
-                       (file-system
-                         (mount-point "/")
-                         (device "/dev/mapper/cryptroot")
-                         (type "ext4")
-                         (dependencies mapped-devices)) %base-file-systems)))
+ ;; The list of file systems that get "mounted".  The unique
+ ;; file system identifiers there ("UUIDs") can be obtained
+ ;; by running 'blkid' in a terminal.
+ (file-systems (cons* (file-system
+                       (mount-point "/boot/efi")
+                       (device (uuid "E342-DBD5"
+                                     'fat32))
+                       (type "vfat"))
+                      (file-system
+                       (mount-point "/")
+                       (device "/dev/mapper/cryptroot")
+                       (type "ext4")
+                       (dependencies mapped-devices)) %base-file-systems)))
