bbd1e8ab62deb8ad35bc264d779aa2c24fdcb053
Ben Juntilla <ben@benjuntilla.com>
2024-12-26 17:02:58 -0700

    feat(kanshi): only set one wallpaper in auxiliary configure script

--- home/dotfiles/.config/kanshi/configure.sh
+++ home/dotfiles/.config/kanshi/configure.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 # Variables
-swaymsg set \$left_wallpaper /home/ben/.config/sway/1.jpg
-swaymsg set \$right_wallpaper /home/ben/.config/sway/2.jpg
-swaymsg set \$left_monitor $1
-swaymsg set \$right_monitor $2
+left_monitor=$1
+right_monitor=$2
+wallpaper_name=${3:-1.jpg}
+
+swaymsg set \$wallpaper "/home/ben/.config/sway/$wallpaper_name"
+swaymsg set \$left_monitor $left_monitor
+swaymsg set \$right_monitor $right_monitor
 
 # Wallpapers
-swaymsg output \$left_monitor background \$left_wallpaper fill
-swaymsg output \$right_monitor background \$right_wallpaper fill
-swaymsg output eDP-2 background \$right_wallpaper fill
+swaymsg output \$left_monitor background \$wallpaper fill
+swaymsg output \$right_monitor background \$wallpaper fill
+
 # Bit depth (this fucks with screensharing)
 # swaymsg output \$left_monitor render_bit_depth 10
 # swaymsg output \$right_monitor render_bit_depth 10
@@ -27,4 +30,3 @@ swaymsg workspace 4 output \$right_monitor
 swaymsg workspace 6 output \$right_monitor
 swaymsg workspace 8 output \$right_monitor
 swaymsg workspace 0 output \$right_monitor
-
