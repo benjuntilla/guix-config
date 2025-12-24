2897e3daed5823081dc64253159ac2bfe6ab5829
Ben Juntilla <ben@benjuntilla.com>
2025-12-23 22:49:48 -0700

    restyle fuzzel

---
+++ home/dotfiles/.local/share/dark-mode.d/fuzzel.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Switch fuzzel to dark theme
+
+mkdir -p ~/.config/fuzzel
+ln -sf fuzzel-dark.ini ~/.config/fuzzel/fuzzel.ini
---
+++ home/dotfiles/.local/share/light-mode.d/fuzzel.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Switch fuzzel to light theme
+
+mkdir -p ~/.config/fuzzel
+ln -sf fuzzel-light.ini ~/.config/fuzzel/fuzzel.ini
