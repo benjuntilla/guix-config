805a7b35c6655e81f94bd4344348597d34fcd3c4
Ben Juntilla <ben@benjuntilla.com>
2026-02-08 11:28:06 -0700

    remove useless channel

--- home/home.scm
+++ home/home.scm
@@ -211,15 +211,6 @@ bind \\ce 'hx .'
                              (openpgp-fingerprint
                               "F695 F39E C625 E081 33B5  759F 0FC6 8703 75EF E2F5"))))
                          (channel
-                          (name 'guix-gaming-games)
-                          (url "https://gitlab.com/guix-gaming-channels/games.git")
-                          ;; Enable signature verification:
-                          (introduction
-                           (make-channel-introduction
-                            "c23d64f1b8cc086659f8781b27ab6c7314c5cca5"
-                            (openpgp-fingerprint
-                             "50F3 3E2E 5B0C 3D90 0424  ABE8 9BDC F497 A4BB CC7F"))))
-                         (channel
                           (name 'nonguix)
                           (url "https://gitlab.com/nonguix/nonguix")
                           (branch "master")
